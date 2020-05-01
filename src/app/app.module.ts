9d0ddd49b97db0b90617aa1ed7c8232c115e19f2
juliscrep <julietascrepnik@gmail.com>
2020-05-01 19:34:47 -0300

    5 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { MenuComponent } from './menu/menu.component';
+import { MenuBarraComponent } from './menu-barra/menu-barra.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, MenuComponent ],
-  bootstrap:    [ AppComponent,MenuComponent ]
+  declarations: [ AppComponent, HelloComponent, MenuComponent, MenuBarraComponent ],
+  bootstrap:    [ AppComponent,MenuComponent,MenuBarraComponent ]
 })
 export class AppModule { }
