2e293369aed3de4ae74bfa004a3ab7a623b6d06e
juliscrep <julietascrepnik@gmail.com>
2020-05-27 11:40:52 -0300

    1 file modified

--- src/app/articulos.service.ts
+++ src/app/articulos.service.ts
@@ -1,29 +1,20 @@
 import { Injectable } from '@angular/core';
 import { articuloFamilia } from './models/articuloFamilia';
 import {HttpClient} from '@angular/common/http'
-import {observable} from 'rxjs'
+import {Observable} from 'rxjs'
 
 @Injectable( {
     providedId: 'root'
   }
 )
 export class ArticulosService {
+  private articulosUrl= 'http://labsys.frc.utn.edu.ar:8080/api/ArticulosFamilias';
 
-  constructor() {}
-  getArticulos(): articuloFamilia[]{
-    return[
-    { IdArticuloFamilia: 1, Nombre: "Accesorios" },
-    { IdArticuloFamilia: 2, Nombre: "Audio" },
-    { IdArticuloFamilia: 3, Nombre: "Celulares" },
-    { IdArticuloFamilia: 4, Nombre: "Cuidado Personal" },
-    { IdArticuloFamilia: 5, Nombre: "Dvd" },
-    { IdArticuloFamilia: 6, Nombre: "Fotografia" },
-    { IdArticuloFamilia: 7, Nombre: "Frio-Calor" },
-    { IdArticuloFamilia: 8, Nombre: "Gps" },
-    { IdArticuloFamilia: 9, Nombre: "Informatica" },
-    { IdArticuloFamilia: 10, Nombre: "Led - Lcd" },
-     { IdArticuloFamilia: 11, Nombre: " Computadora" },
-    ];
+
+  constructor(private http: HttpClient) {}
+  
+  getArticulos(): Observable<IArticulo[]>{
+    return this.http.get<IArticulo[]>(this.articulosUrl);
   }
 
   
